59e89bb4a7dc82b026fa07da3055ba59c8187760
ture23 <47562456+ture23@users.noreply.github.com>
2022-11-15 20:47:01 +0100

    Update api.js

--- client-side/src/api/api.js
+++ client-side/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'http://localhost:5000'});
+const API = axios.create({ baseURL: 'https://invest-for-you.herokuapp.com'});
 
 // ' /  http://localhost:5000 https://invest-for-you.herokuapp.com/
 
